eea6673485dafe506f2a212930cb45bf705b5e21
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-08-10 17:05:19 +0800

    adding daisyui

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -2,3 +2,5 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <h2 class="text-3xl font-bold underline">testing page.svelte</h2>
+<button class="btn btn-primary">
+    testing</button>
